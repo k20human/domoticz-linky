a103a26eebd26785dc95150103211e1039994ee2
k20human <kmat.null@gmail.com>
2017-07-26 02:34:10 +0200

    Add init script

---
+++ execute.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: k20human
+Based on https://github.com/Asdepique777/jeedom_linky
+
+"""
+
+#
+# Copyright (C) 2017 Kévin Mathieu
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+#
+
+import linky
+import json
+import os
+import sys
+import logging
+import datetime
+import url
+import time
+import sqlite3
+import math
+from dateutil.relativedelta import relativedelta
+from logging.handlers import RotatingFileHandler
+
+# Configuration file path
+configurationFile = './config.json'
+
+# Configure logs
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+
+file_handler = RotatingFileHandler('linky.log', 'a', 1000000, 1)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+steam_handler = logging.StreamHandler()
+steam_handler.setLevel(logging.INFO)
+steam_handler.setFormatter(formatter)
+logger.addHandler(steam_handler)
+
+# Check if configuration file exists
+if os.path.isfile(configurationFile):
+    # Import configuration file
+    with open(configurationFile) as data_file:
+        config = json.load(data_file)
+else:
+    logger.error('Your configuration file doesn\'t exists')
+    sys.exit('Your configuration file doesn\'t exists')
+
+# domoticz server & port information
+domoticzServer = config['domoticz_server']
+
+# domoticz IDX
+domoticzIdx = config['domoticz_idx']
+
+# Enedis Login
+enedisLogin = config['login']
+
+# Enedis password
+enedisPassword = config['password']
+
+# Domoticz API
+domoticzApi = url.URL(domoticzServer)
+
+# Export data to Domoticz
+def export_days_values(res):
+    value = res['graphe']['data'][-1]['valeur']
+
+    if value < 0:
+        raise linky.LinkyLoginException('Value is less than 0, error in API')
+
+    # Get value from Domoticz
+    counter = domoticzApi.call({
+        'type': 'devices',
+        'rid': domoticzIdx
+    }).json()
+
+    counterValue = int(float(counter['result'][0]['Counter'].replace(' kWh', '')))
+
+    # Send to Domoticz only if data not send today
+    if counterValue == 0:
+        res = domoticzApi.call({
+            'type': 'command',
+            'param': 'udevice',
+            'idx': domoticzIdx,
+            'svalue': counterValue + int(value * 1000)
+        })
+
+        if res.status_code == 200:
+            logger.info('Data successfully send to Domoticz')
+        else:
+            raise linky.LinkyLoginException('Can\'t add data to Domoticz')
+    else:
+        logger.info('Data already successfully send to Domoticz today')
+
+# Date formatting
+def dtostr(date):
+    return date.strftime("%d/%m/%Y")
+
+def call_enedis_api():
+    token = linky.login(enedisLogin, enedisPassword)
+    today = datetime.date.today()
+
+    res_day = linky.get_data_per_day(token, dtostr(today - relativedelta(days=1, months=1)), dtostr(today - relativedelta(days=1)))
+
+    # If cookie has expired retry
+    if res_day is None:
+        logger.info("Cookie has expired")
+        token = linky.login(enedisLogin, enedisPassword)
+        res_day = linky.get_data_per_day(token, dtostr(today - relativedelta(days=1)), dtostr(today))
+
+    return res_day
+
+# Main script
+def main():
+    try:
+        logger.info("Logging in as %s", enedisLogin)
+        logger.info("Logged in successfully!")
+        logger.info("Retreiving data...")
+
+        # Get datas
+        res_day = call_enedis_api()
+
+        logger.info("Got datas!")
+
+        # Send to Domoticz
+        export_days_values(res_day)
+
+    except linky.LinkyLoginException as exc:
+        logger.error(exc)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
---
+++ linky.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Generates energy consumption JSON files from Enedis (ERDF) consumption data
+collected via their  website (API).
+"""
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import base64
+import requests
+import pickle
+import os
+
+LOGIN_BASE_URI = 'https://espace-client-connexion.enedis.fr'
+API_BASE_URI = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers'
+
+API_ENDPOINT_LOGIN = '/auth/UI/Login'
+API_ENDPOINT_HOME = '/accueil'
+API_ENDPOINT_DATA = '/suivi-de-consommation'
+
+DATA_NOT_REQUESTED = -1
+DATA_NOT_AVAILABLE = -2
+
+COOKIE_FILE_1 = './cookie1'
+COOKIE_FILE_2 = './cookie2'
+
+class LinkyLoginException(Exception):
+    # Thrown if an error was encountered while retrieving energy consumption data.
+    pass
+
+def save_cookies(requests_cookiejar, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(requests_cookiejar, f)
+
+def load_cookies(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+def get_cookies():
+    cookie1 = load_cookies(COOKIE_FILE_1)
+    cookie2 = load_cookies(COOKIE_FILE_2)
+
+    return {'iPlanetDirectoryPro': cookie1, 'JSESSIONID': cookie2}
+
+def login(username, password):
+    # Try to load cookie from file
+    if os.path.isfile(COOKIE_FILE_1):
+        return get_cookies()
+
+    # Login the user into the Linky API.
+    payload = {'IDToken1': username,
+               'IDToken2': password,
+               'SunQueryParamsString': base64.b64encode(b'realm=particuliers'),
+               'encoded': 'true',
+               'gx_charset': 'UTF-8'}
+
+    req = requests.post(LOGIN_BASE_URI + API_ENDPOINT_LOGIN, data=payload, allow_redirects=False)
+    session_cookie = req.cookies.get('iPlanetDirectoryPro')
+
+    if session_cookie is None:
+        raise LinkyLoginException("Login unsuccessful. Check your credentials.")
+
+    # Get second cookie
+    req = requests.get(API_BASE_URI + API_ENDPOINT_HOME, cookies={'iPlanetDirectoryPro': session_cookie}, allow_redirects=False)
+    session_cookie2 = req.cookies.get('JSESSIONID')
+
+    # Store cookies inside file
+    save_cookies(session_cookie, COOKIE_FILE_1)
+    save_cookies(session_cookie2, COOKIE_FILE_2)
+
+    return get_cookies()
+
+
+def get_data_per_hour(token, start_date, end_date):
+    """Retreives hourly energy consumption data."""
+    return _get_data(token, 'urlCdcHeure', start_date, end_date)
+
+
+def get_data_per_day(token, start_date, end_date):
+    """Retreives daily energy consumption data."""
+    return _get_data(token, 'urlCdcJour', start_date, end_date)
+
+
+def get_data_per_month(token, start_date, end_date):
+    """Retreives monthly energy consumption data."""
+    return _get_data(token, 'urlCdcMois', start_date, end_date)
+
+
+def get_data_per_year(token):
+    """Retreives yearly energy consumption data."""
+    return _get_data(token, 'urlCdcAn')
+
+
+def _get_data(token, resource_id, start_date=None, end_date=None):
+    id = 'lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet'
+    prefix = '_' + id + '_'
+
+    # We send the session token so that the server knows who we are
+    cookies = {
+        'iPlanetDirectoryPro': token['iPlanetDirectoryPro'],
+        'JSESSIONID': token['JSESSIONID']
+    }
+
+    payload = {
+        prefix + 'dateDebut': start_date,
+        prefix + 'dateFin': end_date
+    }
+
+    params = {
+        'p_p_id': id,
+        'p_p_lifecycle': 2,
+        'p_p_state': 'normal',
+        'p_p_mode': 'view',
+        'p_p_resource_id': resource_id,
+        'p_p_cacheability': 'cacheLevelPage',
+        'p_p_col_id': 'column-1',
+        'p_p_col_pos': 1,
+        'p_p_col_count': 3
+    }
+
+    req = requests.post(API_BASE_URI + API_ENDPOINT_DATA, allow_redirects=False, cookies=cookies, data=payload,
+                        params=params)
+
+    if req.status_code != 200:
+        os.remove(COOKIE_FILE_1)
+        os.remove(COOKIE_FILE_2)
+        return None
+
+    return req.json()
